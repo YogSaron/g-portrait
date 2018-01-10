9e8674fcf45cc807a38478d94b417ac675f95c32
Logan <shadow_1226@163.com>
2018-01-10 17:45:05 +0800

    Initial commit

---
+++ src/main/java/com/me/GPortraitApplication.java
@@ -0,0 +1,12 @@
+package com.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GPortraitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GPortraitApplication.class, args);
+	}
+}
